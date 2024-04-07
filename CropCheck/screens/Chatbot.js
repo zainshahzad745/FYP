7efae4fe653f5a0977d127fb296cd008df649c7a
zainshahzad745 <zainshahza@gmail.com>
2024-04-07 19:59:00 +0500

    fixed image dimensions (Chatbot)

--- CropCheck/screens/Chatbot.js
+++ CropCheck/screens/Chatbot.js
@@ -18,52 +18,61 @@ import Navbar from "./components/Navbar";
 const backgroundimg = require("../assets/chatbotbg.png");
 const Chatbot = () => {
   return (
-    <View style={styles.container}>
-        <ImageBackground
-        source={backgroundimg}
-        style={styles.background}>
-            <View style={styles.textView}>
-            <Text style={styles.text}>AI Chatbot</Text>
-            </View>
-            <View style={styles.chat}>
-                <Chatbox />
-            </View>
-            <View style={styles.navContainer}>
-                <Navbar />
-            </View>
-        </ImageBackground>
-    </View>
+    // <View style={styles.container}>
+    <ImageBackground
+      source={backgroundimg}
+      style={{
+        width: Dimensions.get("window").width,
+        height: Dimensions.get("window").height,
+        resizeMode: "stretch",
+      }}
+    >
+      <View style={styles.textView}>
+        <Text style={styles.text}>AI Chatbot</Text>
+      </View>
+      <View style={styles.chat}>
+        <Chatbox />
+      </View>
+      <View style={styles.navContainer}>
+      <Navbar />
+      </View>
+    </ImageBackground>
+    // </View>
   );
 };
 
 const windowWidth = Dimensions.get("window").width;
 
 const styles = StyleSheet.create({
-    container: {
-      height: "100%",
-      display: "flex",
-      // backgroundColor:'black',
-      // flex: 1,
-      justifyContent: "center",
-    },
-    navContainer: {
-        marginTop: "2%",
-    },
-    background: {
-        flex: 1,
-        resizeMode: "contain",
-    },
-    text: {
-        textAlign: "center",
-        fontSize: 32,
-        fontWeight: "bold",
-    },
-    textView: {
-        marginTop: "12%",
-    },
-    chat: {
-        height: "82%",
-    }
+  container: {
+    //   height: "100%",
+    //   width: '100%',
+    display: "flex",
+    backgroundColor: "black",
+    // flex: 1,
+    justifyContent: "center",
+  },
+  navContainer: {
+    marginTop: "35%",
+  },
+  background: {
+    flex: 1,
+    resizeMode: "contain",
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+  textView: {
+    marginTop: "15%",
+  },
+  chat: {
+    height: "60%",
+    marginTop: "15%",
+    // padding: "5%",
+    // backgroundColor: "white",
+  },
 });
 
 export default Chatbot;
