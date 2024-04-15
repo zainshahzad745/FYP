3b0d828a5d3aa8039000311a9d4524879a62074e
theshade24 <ifra.mahmood24@gmail.com>
2024-04-15 12:16:36 +0500

    15-04-24 12pm

--- CropCheck/screens/WaterModule.js
+++ CropCheck/screens/WaterModule.js
@@ -8,6 +8,18 @@ const backgroundimg = require("../assets/backgroundimg.jpg");
 
 const WaterModule = () => {
   const [isModalVisible, setIsModalVisible] = useState(false); // State for plant modal visibility
+  const [isDataModalVisible, setIsDataModalVisible] = useState(false); //State for data modal
+  const hideModal = (whichModal, delay) => {
+    setTimeout(() => {
+      whichModal(false);
+    }, delay);
+  };
+
+  const showModal = (whichModal, delay)  => {
+    setTimeout(() => {
+      whichModal(true);
+    }, delay);
+  };
 
   return (
     <View style={{width: Dimensions.get("window").width,
@@ -39,12 +51,92 @@ const WaterModule = () => {
           <Text style={{fontSize: 18,}}>Calculate Now!</Text><Image source={require("../assets/water.png")}></Image>
         </TouchableOpacity>
 
-        {/* 1st Modal */}
+        {/* Snail Modal */}
         <Modal 
           animationType="slide"
           transparent={true}
           visible={isModalVisible}
-          onRequestClose={() => setIsModalVisible(false)}></Modal>
+          onShow={() => { hideModal(setIsModalVisible,3000), showModal(setIsDataModalVisible,4000); }}
+          >
+            <View style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+              height: "60%",
+              width: "100%",
+              marginVertical: "12%",
+            }}>
+              <View style={{
+                backgroundColor: "#A5CEA7",
+                padding: 8,
+                borderRadius: 28,
+              }}>
+                  <Text style={{
+                    marginTop: "8%",
+                    fontSize: 22,
+                    fontWeight: "bold",
+                  }}>Hold Tight!</Text>
+                  <Text style={{
+                    fontSize: 20,
+                    fontWeight: "bold",
+                  }}>Fetching Temperature and Humidity Levels</Text>
+                  <Image source={require("../assets/snail.png")} style={{
+                    width: 300,
+                    height: 300,
+                    resizeMode: "contain",
+                    marginHorizontal: "3%",
+                  }}></Image>
+              </View>
+            </View>
+          </Modal>
+
+          {/* Data Modal */}
+          <Modal
+          animationType="slide"
+          transparent={false}
+          visible={isDataModalVisible}
+          onShow={() => { hideModal(setIsDataModalVisible, 4000); }}
+          >
+            <View style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+              height: "60%",
+              width: "100%",
+              marginVertical: "12%",
+              // backgroundColor: "red",
+            }}>
+              <View style={{
+                backgroundColor: "#A5CEA7",
+                padding: 8,
+                borderRadius: 28,
+                width: "80%",
+              }}>
+                <Text style={{fontSize: 22, fontWeight: "bold",}}>Data</Text>
+                <View style={{flexDirection: "row",}}>
+                  <Text style={{fontSize: 20,}}>Temperature</Text>
+                  <Image source={require("../assets/temp.png")}></Image>
+                  <Text>Result</Text>
+                </View>
+                <View style={{flexDirection: "row",}}>
+                  <Text style={{fontSize: 20,}}>Humidity</Text>
+                  <Image source={require("../assets/humidity.png")}></Image>
+                  <Text>Result</Text>
+                </View>
+                <View style={{flexDirection: "row",}}>
+                  <Text style={{fontSize: 20,}}>Pot Diameter</Text>
+                  <Image source={require("../assets/pot.png")}></Image>
+                  <Text>Input</Text>
+                </View>
+                <View style={{flexDirection: "row",}}>
+                  <Text style={{fontSize: 20,}}>Location</Text>
+                  <Image source={require("../assets/location.png")}></Image>
+                  <Text>Input</Text>
+                </View>
+                <TouchableOpacity></TouchableOpacity>
+              </View>
+            </View>
+          </Modal>
       </View>
       <View>
         <Navbar />
