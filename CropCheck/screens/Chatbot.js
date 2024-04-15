addb32e0f272e9c87faf4591afc7aceeb48488da
theshade24 <ifra.mahmood24@gmail.com>
2024-04-15 06:37:33 +0500

    to revert

--- CropCheck/screens/Chatbot.js
+++ CropCheck/screens/Chatbot.js
@@ -4,13 +4,9 @@ import {
   TextInput,
   TouchableOpacity,
   Text,
-  Image,
-  Button,
-  Alert,
   ImageBackground,
   Dimensions,
   StyleSheet,
-  ViewComponent,
 } from "react-native";
 import Chatbox from "./components/Chatbox";
 import Navbar from "./components/Navbar";
@@ -18,61 +14,47 @@ import Navbar from "./components/Navbar";
 const backgroundimg = require("../assets/chatbotbg.png");
 const Chatbot = () => {
   return (
-    // <View style={styles.container}>
     <ImageBackground
       source={backgroundimg}
       style={{
         width: Dimensions.get("window").width,
         height: Dimensions.get("window").height,
         resizeMode: "stretch",
-      }}
+        }}
     >
-      <View style={styles.textView}>
-        <Text style={styles.text}>AI Chatbot</Text>
-      </View>
-      <View style={styles.chat}>
-        <Chatbox />
-      </View>
-      <View style={styles.navContainer}>
-      <Navbar />
-      </View>
+        <View style={styles.textView}>
+            <Text style={styles.text}>AI Chatbot</Text>
+        </View>
+        <View style={styles.chat}>
+            <Chatbox />
+        </View>
+        <View style={styles.navContainer}>
+            <Navbar />
+        </View>
     </ImageBackground>
-    // </View>
-  );
+    );
 };
 
 const windowWidth = Dimensions.get("window").width;
 
 const styles = StyleSheet.create({
-  container: {
-    //   height: "100%",
-    //   width: '100%',
-    display: "flex",
-    backgroundColor: "black",
-    // flex: 1,
-    justifyContent: "center",
-  },
-  navContainer: {
-    marginTop: "35%",
-  },
-  background: {
-    flex: 1,
-    resizeMode: "contain",
-  },
-  text: {
-    textAlign: "center",
-    fontSize: 32,
-    fontWeight: "bold",
-  },
-  textView: {
-    marginTop: "15%",
-  },
-  chat: {
-    height: "60%",
-    marginTop: "15%",
-    // padding: "5%",
-    // backgroundColor: "white",
-  },
+    navContainer: {
+        marginTop: "35%",
+    },
+    text: {
+        textAlign: "center",
+        fontSize: 32,
+        fontWeight: "bold",
+    },
+    textView: {
+        marginTop: "15%",
+    },
+    chat: {
+        height: "60%",
+        marginTop: "15%",
+        // padding: "5%",
+        // backgroundColor: "white",
+    },
 });
 
 export default Chatbot;
--- CropCheck/screens/WaterModule.js
+++ CropCheck/screens/WaterModule.js
@@ -1,11 +1,55 @@
 // WaterModule.js
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, View, Text, ImageBackground, Image, Button, Dimensions, TouchableOpacity } from 'react-native';
+import Modal from "react-native-modal";
+import Navbar from "./components/Navbar";
+
+const backgroundimg = require("../assets/backgroundimg.jpg");
 
 const WaterModule = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false); // State for plant modal visibility
+
   return (
-    <View>
-      <Text>This is the new screen!</Text>
+    <View style={{width: Dimensions.get("window").width,
+        height: "100%",
+        display: "flex", justifyContent: "center",}}>
+      <ImageBackground
+        source={backgroundimg}
+        style={{
+          resizeMode: "stretch",
+          flex: 1,
+        }}
+      > 
+      <View style={{justifyContent: "center", height: "96%", paddingLeft: "5%", paddingRight: "5%",}}>
+        <Text style={{fontSize: 22, marginBottom: 10, fontWeight:"bold", marginHorizontal: "5%",}}>
+          Elevating Plant Health with Tailored Watering Guidelines
+        </Text>
+        <TouchableOpacity onPress={() => setIsModalVisible(true)} style={{
+            backgroundColor: "green", // Green background color
+            opacity: 0.8, // Semi-transparent
+            borderRadius: 50, // Custom border radius
+            width: "76%", // Custom width
+            height: "8%",
+            marginHorizontal: "12%",
+            marginTop: "12%", // Custom height
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "row",
+          }}>
+          <Text style={{fontSize: 18,}}>Calculate Now!</Text><Image source={require("../assets/water.png")}></Image>
+        </TouchableOpacity>
+
+        {/* 1st Modal */}
+        <Modal 
+          animationType="slide"
+          transparent={true}
+          visible={isModalVisible}
+          onRequestClose={() => setIsModalVisible(false)}></Modal>
+      </View>
+      <View>
+        <Navbar />
+      </View>
+      </ImageBackground>
     </View>
   );
 };
