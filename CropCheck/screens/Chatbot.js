4b0e5e680784fe5b71921e10bc4d11970759a8f6
zainshahzad745 <zainshahza@gmail.com>
2024-04-16 13:59:30 +0500

    fixed keyboard

--- CropCheck/screens/Chatbot.js
+++ CropCheck/screens/Chatbot.js
@@ -2,16 +2,19 @@ import React from "react";
 import {
   View,
   TextInput,
-  TouchableOpacity,
   Text,
   ImageBackground,
   Dimensions,
   StyleSheet,
+  KeyboardAvoidingView,
+  Keyboard,
+  Platform,
 } from "react-native";
 import Chatbox from "./components/Chatbox";
 import Navbar from "./components/Navbar";
 
 const backgroundimg = require("../assets/chatbotbg.png");
+
 const Chatbot = () => {
   return (
     <ImageBackground
@@ -20,41 +23,44 @@ const Chatbot = () => {
         width: Dimensions.get("window").width,
         height: Dimensions.get("window").height,
         resizeMode: "stretch",
-        }}
+      }}
     >
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={{ flex: 1 }}
+      >
         <View style={styles.textView}>
-            <Text style={styles.text}>AI Chatbot</Text>
+          <Text style={styles.text}>AI Chatbot</Text>
         </View>
         <View style={styles.chat}>
-            <Chatbox />
+          <Chatbox />
         </View>
         <View style={styles.navContainer}>
-            <Navbar />
+          <Navbar />
         </View>
+      </KeyboardAvoidingView>
     </ImageBackground>
-    );
+  );
 };
 
 const windowWidth = Dimensions.get("window").width;
 
 const styles = StyleSheet.create({
-    navContainer: {
-        marginTop: "35%",
-    },
-    text: {
-        textAlign: "center",
-        fontSize: 32,
-        fontWeight: "bold",
-    },
-    textView: {
-        marginTop: "15%",
-    },
-    chat: {
-        height: "60%",
-        marginTop: "15%",
-        // padding: "5%",
-        // backgroundColor: "white",
-    },
+  navContainer: {
+    marginTop: "35%",
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+  textView: {
+    marginTop: "15%",
+  },
+  chat: {
+    height: "60%",
+    marginTop: "15%",
+  },
 });
 
 export default Chatbot;
