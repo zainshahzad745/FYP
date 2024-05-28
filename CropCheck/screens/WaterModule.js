11142e79e332e3a391aea60551a2867c62e8a718
zainshahzad745 <54791999+zainshahzad745@users.noreply.github.com>
2024-05-28 09:59:40 +0000

    Update WaterModule.js

--- CropCheck/screens/WaterModule.js
+++ CropCheck/screens/WaterModule.js
@@ -151,8 +151,8 @@ const WaterModule = () => {
   };
 
   return (
-    <View style={{width: Dimensions.get("window").width,
-        height: Dimensions.get("window").height,
+    <View style={{width: "100%",
+        height: "100%"
         display: "flex", justifyContent: "center",}}>
       <ImageBackground
         source={backgroundimg}
